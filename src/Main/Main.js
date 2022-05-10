10c8e4e6481cb7b61c6a869c7135a250a843a2a4
Actina <rikonvald@gmail.com>
2022-05-10 21:35:31 +0300

    optimize main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,40 +1,39 @@
-import s from "./main.module.sass"
-import {wrapper} from '../presets/wrapper.module.sass'
+import s from "./main.module.sass";
+import { wrapper } from "../presets/wrapper.module.sass";
 
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const Main = () => {
-    const [slider, setSlider] = useState()
+  const [slider, setSlider] = useState();
 
-	const settingsSlider = {
-        infinite: false,
-        easing: "ease",
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-		swipeToSlide: true,
-        // beforeChange: (current, next)  => (setActiveIndicator(next))
-    }
-	const ref = useRef()
+  const settingsSlider = {
+    infinite: false,
+    easing: "ease",
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+    // beforeChange: (current, next)  => (setActiveIndicator(next))
+  };
+  const slides = [
+	<div className={s.main__slide}>хуй</div>,
+	<div className={s.main__slide}>пизда</div>
+  ]
 
-	return (
-		<div className={`${s.main} ${wrapper}`}>
-			<div className={s.main__slider}>
-				<Slider
-					{...settingsSlider}
-					ref={slider => (setSlider(slider))}
-				>
-					<div className={s.main__slide}>хуй</div>
-					<div className={s.main__slide}>пизда</div>
-				</Slider>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={`${s.main} ${wrapper}`}>
+      <div className={s.main__slider}>
+        <Slider {...settingsSlider} ref={(slider) => setSlider(slider)}>
+          {slides}
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
