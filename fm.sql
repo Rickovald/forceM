240fe3673259f991eede667d32fb2d3fe1aecef2
Rick <rikonvald@gmail.com>
2022-05-12 11:25:27 +0300

    12

--- fm.sql
+++ fm.sql
@@ -80,20 +80,14 @@ CREATE TABLE `concert_program` (
   `comments` varchar(100) NOT NULL,
   `place` int NOT NULL,
   `concert_name` varchar(30) NOT NULL,
-  `tortan` tinyint(1) NOT NULL,
-  `neru` tinyint(1) NOT NULL,
-  `filar` tinyint(1) NOT NULL,
-  `nikonn` tinyint(1) NOT NULL,
-  `hira` tinyint(1) NOT NULL,
-  `ricko` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `concert_program`
 --
 
-INSERT INTO `concert_program` (`id`, `name`, `cover`, `difficulty`, `comments`, `place`, `concert_name`, `tortan`, `neru`, `filar`, `nikonn`, `hira`, `ricko`) VALUES
-(1, 'Force-Minor - Кривой Роцк', 0, 3, 'Трудно держать ритм на слабой доле. Насть, пропиши барабаны', 1, 'Тот куда Тортан позвал', 1, 0, 0, 0, 0, 1);
+INSERT INTO `concert_program` (`id`, `name`, `cover`, `difficulty`, `comments`, `place`, `concert_name`) VALUES
+(1, 'Force-Minor - Кривой Роцк', 0, 3, 'Трудно держать ритм на слабой доле. Насть, пропиши барабаны', 1, 'Тот куда Тортан позвал');
 
 -- --------------------------------------------------------
 
@@ -115,7 +109,7 @@ CREATE TABLE `discography` (
 --
 
 INSERT INTO `discography` (`id`, `name`, `year`, `songlist`, `href`, `image`) VALUES
-(1, 'Debut', 2022, 'debutlist', 'https://vk.com/music/playlist/-184530709_1', 'debut.webp');
+(1, 'Debut', 2022, 'debutlist', 'https://vk.com/music/playlist/-184530709_1', '/images/debut.webp');
 
 --
 -- Индексы сохранённых таблиц
