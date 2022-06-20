85995caa7954cb7d73a621e0e3db7c5bfbf3b581
Actina <rikonvald@gmail.com>
2022-06-20 20:03:55 +0300

    hosting settings

--- api/config.js
+++ api/config.js
@@ -2,9 +2,9 @@ const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
     host: "127.0.0.1",
-    user: "root",
-    password: "root",
-    database: "fm",
+    user: "a0687392_fm",
+    password: "Sl1dQ60bIlNRdWzR",
+    database: "a0687392_fm",
     connectionLimit: 10,
   },
   listPerPage: 10,
